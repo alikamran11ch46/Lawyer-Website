e1d1a5a413626921a456b94181548b4a5116d8d9
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-08-07 21:28:35 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import HomePage from "@/components/HomePage/HomePage";
+import Aboutus from "@/components/HomePage/aboutus/Aboutus";
+import HomePage from "@/components/HomePage/Banner/HomePage";
+import Sec3 from "@/components/HomePage/sec3/Sec3";
+import Skins from "@/components/HomePage/skins/Skins";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <HomePage/>
+      <Skins/>
+      <Aboutus/>
+      <Sec3/>
     </div>
   );
 }
