9c9e83e3cbb83bdb9e0ab07b7e7a2b4d3e41d29b
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-08-08 20:16:04 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter , Lalezar } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
         <main>
           {children}
         </main>
+         <Footer/>
       </body>
     </html>
   );
