76c6f92a57981c2d7833c2ec476ac851b05713ea
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-08-10 21:00:51 +0330

    Add files via upload

---
+++ app/about-us/page.tsx
@@ -0,0 +1,10 @@
+import AboutsUs from '@/components/pageSections/AboutsUs'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <AboutsUs/>
+    </div>
+  )
+}
---
+++ app/contact-us/page.tsx
@@ -0,0 +1,10 @@
+import ContactUs from '@/components/pageSections/ContactUs'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <ContactUs/>
+    </div>
+  )
+}
