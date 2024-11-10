81eaa63747e207b1ebc555da841bd620100fddf1
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2024-11-10 22:23:16 +0330

    Update HomePage.tsx

--- components/HomePage/Banner/HomePage.tsx
+++ components/HomePage/Banner/HomePage.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
             
             <div className=' w-full h-full  absolute  bg-[#0202026e]'/>
             <div className='  relative flex flex-col gap-y-2 z-40 px-[26px] lg:px-[90px] xl:px-[130px] 2xl:px-[160px]'>
-                <h2 className='text-white text-[24px] lg:text-[40px] font-bold'>حسین کاویانی</h2>
+                <h2 className='text-white text-[24px] lg:text-[40px] font-bold'>پارسا شمس</h2>
                 <h3 className='text-white text-[26px] lg:text-[42px] font-bold'>وکیل پایه یک دادگستری</h3>
                 <p className='text-white text-[11px] lg:text-[15px] '>وکیل پایه یک دادگستری متخصص در دعاوی قضایی شامل: زمینه های حقوقی-کیفری-ثبتی و غیره</p> 
                 <button className=' w-[40%] relative py-1 mt-3  group lg:py-3 text-white rounded-md border border-[#D4AF37]'>
